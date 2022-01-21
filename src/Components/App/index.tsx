db7e0a83f639773390f000a9099c434018937c00
adamt <thompson.ad94@gmail.com>
2022-01-21 17:00:22 +0000

    added css themeing

--- src/Components/App/index.tsx
+++ src/Components/App/index.tsx
@@ -8,8 +8,18 @@ import Intro from '../Intro';
 import Projects from '../Projects';
 
 function App() {
+  //so css theme can be applied from body tag
+  React.useEffect(() => {
+    const body = document.querySelector("body");
+    document.body.classList.add("App");
+    document.body.classList.add("light");
+    return () => {
+      body?.classList.remove("App");
+      body?.classList.remove("light");
+    }
+  });
   return (
-    <div className="App">
+    <div className="main">
       <Header />
       <NavBar />
       <Bubbles />
