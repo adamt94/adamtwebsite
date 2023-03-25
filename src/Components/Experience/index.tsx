d485a31064857ac4ee008248aebe648fde7e3798
Adam Thompson <adamthompson@Adams-MacBook-Air.local>
2023-03-25 10:35:22 +0000

    added experience section

--- src/Components/Experience/index.tsx
+++ src/Components/Experience/index.tsx
@@ -1,12 +1,19 @@
+import styles from "./index.module.scss";
+
 export default function Experience() {
+  const date = new Date();
+  const year = date.getFullYear();
+  const month = date.toLocaleString("default", { month: "long" });
   return (
-    <section id="experience">
+    <section id="experience" className={styles.experience}>
       <h1>Experience</h1>
       <div className="subline"></div>
       <p>
         <h2>Front End Engineer</h2>
         <h3>Smart Pension</h3>
-        <p>2022 to current</p>
+        <p>
+          September 2022 to {month} {year}
+        </p>
         <p>
           Front end developer working on a new CMS using modern web technologies
           such as React, Graphql and Remix. Built for desktop and mobile
@@ -15,19 +22,30 @@ export default function Experience() {
         </p>
       </p>
       <p>
-        <h2>Software Engineer</h2>
+        <h2>Mid Software Engineer</h2>
         <h3>BBC Global News</h3>
-        <p>2021 to 2022</p>
+        <p>August 2021 to September 2022</p>
+        <p>
+          During the time a BBC I moved up to a mid-level engineer and also
+          worked some newer projects. One of the projects was to build modern
+          bbc home page, using a modern tech stack, using NextJS, typescript and
+          allowing the homepage to be more dynamic and customizable. Also worked
+          on Pagebuilder which allowed advertisers and sponsers to build great
+          looking article pages they could use to show off their product or
+          content.
+        </p>
+      </p>
+      <p>
+        <h2>Junior Software Engineer</h2>
+        <h3>BBC Global News</h3>
+        <p>January 2021 to August 2021</p>
         <p>
           Working on a wide variety of projects and tech stacks (legacy and
           modern) built for millions of users. Succesfully released BBC travel,
           culture, future and worklife to new design and techstack (React,
           redux, typescript, node, aws). Worked on BBC reel a premium video and
           short documentary platform for readers to help look beyond their
-          world. Worked on page builder website, a tool for sponsers and
-          advetisement to generate great looking pages for their content. Create
-          a generic logging system to allow for better debugging in aws across
-          the projects.
+          world.
         </p>
       </p>
       <p>
